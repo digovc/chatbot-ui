caeb335cef686a089b61f00a7f98ddebbf694613
Rodrigo Daros <rodrigo@mobilesaude.com.br>
2023-12-11 18:58:46 -0300

    Ajuste.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // i18n,
+  i18n,
   reactStrictMode: true,
   basePath: '/chatbot-ui',
   assetPrefix: '/chatbot-ui/',
